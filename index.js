cd5b9a98a021cf43600888c472f3995d8a157ec4
Leonardo Pucheta <pucheta.leonardo@gmail.com>
2022-09-07 02:51:18 +0200

    simuladorInteractivo

--- index.js
+++ index.js
@@ -1,68 +1,25 @@
 
-//__________________________________________________________________________________ CONSIGNA 1 de CODER !!!!! 
+// //__________________________________________________________________________________SIMULADOR INTERACTIVO CODER 
+let servicios = Number (prompt("Ingrese una cantidad de servicios para sumar"));
+let resultado = 0;
 
-let numero1 = Number(prompt ("Ingrese un número al azar"));
+function sumaServicios (numero){
+    resultado = resultado + numero;
+}
 
-if (numero1 > 1000) {
-    alert("Ese número es demasiado grande")
-};
-
-// ______Pedir un texto mediante prompt, y si es igual a "Hola" mostrar un alerta por consola.
-//__________________________________________________________________________________ CONSIGNA 2 de CODER !!!!! 
-
-let texto = prompt ("Hola, Buenos días");
-
-if (texto == "Hola") {
-    alert("El texto ingresado es: " + texto)
-};
-
-// ______Pedir un número por prompt y evaluar si está entre 10 y 50. En caso positivo mostrar un alert.
-//__________________________________________________________________________________ CONSIGNA 3 de CODER !!!!! 
-
-let numero2 = Number(prompt ("Ingrese otro número"));
-
-if ((numero2 > 10) && (numero2 < 50)) {
-    alert("Tu número esta entre 10 y 50")
-};
-
-// _____Pedir número mediante prompt y sumarle otro número en cada repetición,realizando una salida por cada resultado.
-//____________________________________________________________________________________________ CONSIGNA 4 de CODER !!!!! 
-
-let numero3 = Number(prompt ("Ingrese un número base"))
-
-
-for(let i=0; i < 6; i++){
+if (servicios > 0){
+    for   (let i=0; i<servicios; i++){
+        let valorServicio = Number (prompt("Ingrese el valor del servicio"));
+        sumaServicios (valorServicio);
+    }
+}else {
+    alert ("Por favor, ingresa alguna cantidad de servicios para sumar")
+}
     
-    alert(numero3 + i);
-}
 
-// ______Pedir un texto mediante prompt, concatenar un valor en cada repetición, realizando una salida por cada resultado, hasta que se ingresa “ESC”.
-//____________________________________________________________________________________________ CONSIGNA 5 de CODER !!!!! 
-
-let texto1 = prompt("Escribe el nombre de una fruta")
-let texto2 = " "
-
-do{
-    texto2 = prompt ("Nombre diferentes caracteristicas de esa fruta - (Escribe ESC para finalizar)")
-    resultado = texto1 + " " + texto2
-    console.log(resultado)
-}
-while(texto2 != "ESC")
+    alert("El valor total de los servicios es " + resultado)
 
 
-// ______Pedir un número por prompt, repetir la salida del mensaje “Hola” la cantidad de veces ingresada.
-//____________________________________________________________________________________________ CONSIGNA 6 de CODER !!!!! 
-
-let numero4 = Number(prompt("Ingresar un número del 1 al 10"))
-if(numero4 > 10){ 
-    alert("Aprende a respetar consignas")
-}
-
-else{
-for(let i=0; i<numero4 ; i++){
-    alert("Hola")
-}
-}
 
 
 
